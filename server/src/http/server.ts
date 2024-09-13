5505488108fb823aee2296ab62a10fcf39cdaad4
salespaulo <salespaulo08@gmail.com>
2024-09-13 19:38:55 -0300

    Aula 03 - Integrando backend p/ tarefas pendentes

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,38 +1,43 @@
-import fastify from "fastify";
-import cors from "@fastify/cors"
+import fastify from 'fastify'
+import cors from '@fastify/cors'
 import {
     createGoal,
     createGoalCompletion,
     createGoalCompletionSchema,
     createGoalSchema,
     getWeekPendingGoal,
-    getWeekSummary
+    getWeekSummary,
 } from '../goal'
 
 const app = fastify()
-app.register(cors, { origin: "*" })
+app.register(cors, { origin: '*' })
 
 app.post('/goals', async req => {
-    const goal = createGoalSchema.parse(req.body)
-    await createGoal({
-        title: goal.title,
-        desireWeeklyFrequency: goal.desireWeeklyFrequency
-    })
+  const goal = createGoalSchema.parse(req.body)
+  await createGoal({
+    title: goal.title,
+    desireWeeklyFrequency: goal.desireWeeklyFrequency,
+  })
 })
 
 app.post('/goals/complete', async req => {
+  try {
     const goal = createGoalCompletionSchema.parse(req.body)
     return createGoalCompletion(goal)
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 app.get('/goals/pending', async () => {
-    return await getWeekPendingGoal()
+  return await getWeekPendingGoal()
 })
 
 app.get('/goals/summary', async () => {
-    return await getWeekSummary()
+  return await getWeekSummary()
 })
 
-app.listen({ port: 3333 })
-    .then(() => console.log("HTTP Server Running"))
-    .catch(err => console.error(err))
+app
+  .listen({ port: 3333 })
+  .then(() => console.log('HTTP Server Running'))
+  .catch(err => console.error(err))
